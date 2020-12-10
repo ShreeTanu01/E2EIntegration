662ff9deaa0a3e0fff19c0d7dd9a749d03339a1d
ttembhur <ttembhur@DIN30000644.corp.capgemini.com>
2020-12-10 17:40:48 +0530

    adding project

---
+++ mavenpractise/src/test/java/com/org/mavenlearning/mavenpractise/AppTest.java
@@ -0,0 +1,20 @@
+package com.org.mavenlearning.mavenpractise;
+
+import static org.testng.Assert.assertTrue;
+
+import org.testng.annotations.Test;
+
+//import static org.junit.Assert.assertTrue;
+
+//import org.junit.Test;
+
+
+public class AppTest 
+{
+   
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+    	System.out.println("Running Test 1");
+    }
+}
---
+++ mavenpractise/src/test/java/com/org/mavenlearning/mavenpractise/HelloTest.java
@@ -0,0 +1,19 @@
+package com.org.mavenlearning.mavenpractise;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+public class HelloTest {
+@Test
+public void testLogin()
+{
+System.out.println("Running Test 2");
+//System.setProperty("webdriver.chrome.driver", ".\\Drivers\\chromedriver.exe");
+//ChromeDriver driver = new ChromeDriver();
+//driver.get("http://www.google.com");
+//driver.findElementByXPath("/html/body/header/div/aside/div/nav/ul/li[2]/a").click();
+//driver.findElementByXPath("//*[@id=\"i0116\"]").sendKeys("tanushree.tembhurnikar@capgemini.com");
+//driver.findElementByXPath("//*[@id=\"idSIButton9\"]").click();
+//driver.findElementByXPath("//*[@id=\"mectrl_headerPicture\"]").click();
+//driver.findElementByXPath("//*[@id=\"mectrl_body_signOut\"]").click();
+//driver.quit();
+	}
+}
